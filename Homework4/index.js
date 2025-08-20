17a43279871f109ee527a9bddf02449c17042645
Rikyo <rikyomacbook@Rikyos-MacBook-Air.local>
2025-08-20 15:13:20 -0500

    updating files

---
+++ Homework4/index.js
@@ -0,0 +1,5 @@
+// CheckBack Later//
+
+// const genres_items = ["Action", "Battle", "Fantasy", "Psychological"];
+// const genres_div = document.getElementById("Genres");
+// genres_div.innerHTML = genres_items.join
